d32bf51a276ec26abef834a6fe1d6bdc04be7e1c
Jvr <109031036+Jvr2022@users.noreply.github.com>
2025-03-12 17:32:21 +0100

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss')(),
+    require('autoprefixer')(),
+  ],
 };
-
-export default config;
